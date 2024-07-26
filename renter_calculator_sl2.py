736075e2b1aeec3c507c68d41923443d05f616b6
Matt Johnston <matt@tallpixels.com>
2024-07-25 17:46:39 -0700

    removed unnecessary line

--- renter_calculator_sl2.py
+++ renter_calculator_sl2.py
@@ -4,7 +4,6 @@ def main():
     # Set up the main title and description
     st.title("Rent-to-Own Calculator")
     st.write("This tool enables you to determine the equity you will own in your home over time, calculate monthly mortgage payments, and gives a great comparison between buying and renting a place.")
-    add_vertical_space(2)
 
 if __name__ == "__main__":
     main()
